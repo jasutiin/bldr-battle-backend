88f5cc032e09b625c8ae7a8db1a5d1d81a8c1d59
justine <justinemangaliman@gmail.com>
2026-01-03 14:55:57 -0700

    connect admin router to fastapi app

--- server/main.py
+++ server/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from routers import users, climbs, comments
+from routers import users, climbs, comments, admin
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(climbs.router)
 app.include_router(comments.router)
+app.include_router(admin.router)
