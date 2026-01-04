fcd9792a2951f441c7765597772deaf4f9bbb54f
justine <justinemangaliman@gmail.com>
2026-01-03 17:20:02 -0700

    implement db queries for climbs, users, comments

---
+++ server/db/repos/climbs.py
@@ -0,0 +1,49 @@
+from db.models import Climb
+from db.db import engine
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+def get_climb(id: int):
+  with Session(engine) as session:
+    statement = select(Climb).where(Climb.id == id)
+    climb = session.execute(statement).scalar_one_or_none()
+    return climb
+
+
+def get_climbs_by_user(user_id: int):
+  with Session(engine) as session:
+    statement = select(Climb).where(Climb.user_id == user_id)
+    climbs = session.execute(statement).scalars().all()
+    return climbs
+
+
+def get_verified_climbs():
+  with Session(engine) as session:
+    statement = select(Climb).where(Climb.verified == True)
+    climbs = session.execute(statement).scalars().all()
+    return climbs
+
+
+def get_unverified_climbs():
+  with Session(engine) as session:
+    statement = select(Climb).where(Climb.verified == False)
+    climbs = session.execute(statement).scalars().all()
+    return climbs
+
+
+def create_climb(user_id: int, title: str, description: str, video_url: str):
+  with Session(engine) as session:
+    climb = Climb(user_id=user_id, title=title, description=description, video_url=video_url)
+    session.add(climb)
+    session.commit()
+    session.refresh(climb)
+    return climb
+
+
+def update_climb_verification(id: int, verified: bool):
+  with Session(engine) as session:
+    session.query(Climb).filter(Climb.id == id).update({"verified": verified})
+    session.commit()
+    statement = select(Climb).where(Climb.id == id)
+    climb = session.execute(statement).scalar_one_or_none()
+    return climb
--- server/db/repos/users.py
+++ server/db/repos/users.py
@@ -8,3 +8,12 @@ def get_user(id: int):
     statement = select(User).where(User.id == id)
     user = session.execute(statement).scalar_one_or_none()
     return user
+
+
+def create_user(username: str, email: str):
+  with Session(engine) as session:
+    user = User(username=username, email=email)
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
